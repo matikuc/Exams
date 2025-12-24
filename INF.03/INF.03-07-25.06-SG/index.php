f18c914056441101fe8ae5b68f3d32e96ff1d9cb
matikuc <mati.kuc06@gmail.com>
2025-12-24 11:54:23 +0100

    Exams INF.03/EE.09

--- INF.03/INF.03-07-25.06-SG/index.php
+++ INF.03/INF.03-07-25.06-SG/index.php
@@ -1,97 +1,90 @@
-<?php
-    $connect = mysqli_connect("localhost", "root", "", "wyprawy") or die("Błąd");
-?>
 <!DOCTYPE html>
 <html lang="pl">
+
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Biuro turystyczne</title>
     <link rel="stylesheet" href="styl.css">
 </head>
+
 <body>
     <nav>
-        <a href="wczasy.html">Wczasy</a>
-        <a href="wycieczki.html">Wycieczki</a>
-        <a href="allinclusive.html">All Inclusive</a>
-
+        <ul>
+            <li><a href="wczasy.html">Wczasy</a></li>
+            <li><a href="wycieczki.html">Wycieczki</a></li>
+            <li><a href="allinclusive.html">All inclusive</a></li>
+        </ul>
     </nav>
     <main>
         <aside>
             <h3>Twój cel wyprawy</h3>
             <form action="index.php" method="POST">
-                <label for="MiejsceWycieczki">Miejsce wycieczki:<br>
-                <select name="MiejsceWycieczki" id="MiejsceWycieczki"><br>
-                    <?php
-                    $query = "SELECT nazwa FROM miejsca ORDER BY nazwa ASC;";
-                    $result = mysqli_query($connect, $query);
-                    while($row = mysqli_fetch_assoc($result)){
-                        echo "<option value='$row[nazwa]'>$row[nazwa]</option>";
-                    }
-                    
-                    ?>
-                </select>
-                </label><br>
-                <label for="IleDoroslych">Ile dorosłych?<br>
-                    <input type="number" name="IleDoroslych" id="IleDoroslych">
-
-                </label><br>
-                <label for="IleDzieci">Ile dzieci?<br>
-                    <input type="number" name="IleDzieci" id="IleDzieci">
-
-                </label><br>
-                <label for="Termin">Termin<br>
-                    <input type="date" name="Termin" id="Termin">
-
+                <label>
+                    Miejsce wycieczki:<br>
+                    <select name="miejsceWycieczki">
+                        <?php
+                        $connect = mysqli_connect("localhost", "root", "", "wyprawy");
+                        $query = "SELECT nazwa FROM miejsca ORDER BY nazwa ASC;";
+                        $result = mysqli_query($connect, $query);
+                        while ($row = mysqli_fetch_array($result)) {
+                            echo "<option value='{$row['nazwa']}'>{$row['nazwa']}</option>";
+                        }
+                        ?>
+                    </select>
                 </label>
                 <br>
-                <button name="submit">Symulacja ceny</button><br>
-                <h4>Koszt wycieczki</h4>
-                <?php
-                if(isset($_POST['submit'])){
-                    $miejsceWycieczki = $_POST['MiejsceWycieczki'];
-                    $iledoroslych = $_POST['IleDoroslych'];
-                    $iledzieci = $_POST['IleDzieci'];
-                    $termin = $_POST['Termin'];
-                    $query1 = "SELECT `cena` FROM `miejsca` where `nazwa` like '$miejsceWycieczki'; ";
-                    $result1 = mysqli_query($connect,$query1);
-                    $row1 = mysqli_fetch_assoc($result1);
-                    $cena = $row1['cena'];
-                    $koszt = $cena *$iledoroslych + ($cena*0.5)*$iledzieci;
-                    echo "<p> W dniu $termin </p>";
-                    echo "$koszt złotych";
-                }
-                ?>
-
+                <label>
+                    Ile dorosłych?<br>
+                    <input type="number" name="ileDoroslych">
+                </label>
+                <br>
+                <label>
+                    Ile dzieci?<br>
+                    <input type="number" name="ileDzieci">
+                </label>
+                <br>
+                <label>
+                    Termin<br>
+                    <input type="date" name="termin">
+                </label>
+                <br>
+                <button name="submit">Symulacja ceny</button>
             </form>
-
+            <h4>Koszt wycieczki</h4>
+            <?php
+            if (isset($_POST["submit"])) {
+                $miejsceWycieczki = $_POST["miejsceWycieczki"];
+                $ileDoroslych = $_POST["ileDoroslych"];
+                $ileDzieci = $_POST["ileDzieci"];
+                $termin = $_POST["termin"];
+                $query = "SELECT cena FROM miejsca WHERE nazwa = '$miejsceWycieczki';";
+                $result = mysqli_query($connect, $query);
+                $row = mysqli_fetch_array($result);
+                $cena = $row["cena"] * ($ileDoroslych + ($ileDzieci / 2));
+                echo "<p>W dniu $termin</p>
+                    <p>$cena złotych</p>";
+            }
+            ?>
         </aside>
         <section>
-            <h3>Wycieczki</h3>
+            <h3>Wyczieczki</h3>
             <?php
-            $query2 = "SELECT `nazwa`, `cena`, `link_obraz` FROM `miejsca` WHERE `link_obraz` like '0%'; "; 
-            $result2 = mysqli_query($connect,$query2);
-            while($row2 = mysqli_fetch_assoc($result2)){
-                echo "<div class='wycieczka'>";
-                echo "<img src='$row2[link_obraz]' alt='zdjęcie z wycieczki'>";
-                echo "<h2>$row2[nazwa]</h2>";
-                echo "<p>$row2[cena] </p>";
-                echo "</div>"; }
-
-
-            
+            $query = "SELECT nazwa, cena, link_obraz FROM miejsca WHERE link_obraz LIKE '0%';";
+            $result = mysqli_query($connect, $query);
+            while ($row = mysqli_fetch_array($result)) {
+                echo "<div class='wycieczka'>
+                        <img src='{$row['link_obraz']}' alt='zdjęcie z wycieczki'>
+                        <h2>{$row['nazwa']}</h2>
+                        <p>{$row['cena']}</p>
+                    </div>";
+            }
+            mysqli_close($connect);
             ?>
-
         </section>
     </main>
     <footer>
-        <p>Autor: XXXXXXXXXXX</p>
-
+        <p>Autor: XXXXXXXXXXXXXXXXXXXXX</p>
     </footer>
-    <?php
-    mysqli_close($connect);
-            
-            ?>
-    
 </body>
+
 </html>
